7ede363d4da642980c5c0c5326735b212fee0352
thrio <zeus1447@gmail.com>
2021-01-10 03:24:05 +0900

    拡張メソッドを実装

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+/*
+* 拡張メソッドを用いて独自の処理を実装.
+* 参考: https://kakkoyakakko2.hatenablog.com/entry/2018/06/28/003000
+*/
+
+import * as Twitter from "twitter-api-client";
+
+declare module "twitter-api-client" {
+  interface TwitterClient {
+    // メソッドを追加する時は、ここに型を定義して下で実装
+    getClient(): Twitter.TwitterClient;
+    getTweetById(id: string): Promise<Twitter.StatusesShowById>;
+    getTweetByUrl(url: string): Promise<Twitter.StatusesShowById>;
+    getUserById(user_id: string): Promise<Twitter.UsersShow>;
+    getUserByScreenName(screen_name: string): Promise<Twitter.UsersShow>;
+  }
+}
+Twitter.TwitterClient.prototype.getClient = () => {
+  return new Twitter.TwitterClient({
+    apiKey: process.env.TWITTER_API_KEY as string,
+    apiSecret: process.env.TWITTER_API_SECRET as string,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+}
+Twitter.TwitterClient.prototype.getTweetById = async (id: string) => {
+  return await Twitter.TwitterClient.prototype.tweets.statusesShowById({ id: id, include_entities: true, tweet_mode: "extended" });
+}
+Twitter.TwitterClient.prototype.getTweetByUrl = async (url: string) => {
+  let id = new URL(url).pathname.match(/\d+$/)?.[0] as string;
+  return Twitter.TwitterClient.prototype.getTweetById(id);
+}
+Twitter.TwitterClient.prototype.getUserById = async (user_id: string) => {
+  return await Twitter.TwitterClient.prototype.accountsAndUsers.usersShow({ user_id: user_id });
+}
+Twitter.TwitterClient.prototype.getUserByScreenName = async (screen_name: string) => {
+  return await Twitter.TwitterClient.prototype.accountsAndUsers.usersShow({ screen_name: screen_name });
+}
+
+export default Twitter;
